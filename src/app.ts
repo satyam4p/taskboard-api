8d34c424a923e79848aa8a7b81c70ac06425305a
satyam_kumar_ <satyamkumar343@gmail.com>
2022-08-23 22:07:51 +0530

    adding typescript support

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-const express = require('express');
+import express, { Express, Request, Response  } from "express";
 
 const app = express();
 
 const port = 9000;
 
-app.get('/',(req, res)=>{
+app.get('/',(req: Request, res: Response)=>{
     res.send('taskboard api');
 })
 
