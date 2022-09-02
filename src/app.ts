ecf9e4bf2d7e2307f5ef19f164f1b4910b808207
satyam_kumar_ <satyamkumar343@gmail.com>
2022-09-02 20:42:08 +0530

    basic try catch validations added

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ class App{
     }
 
     private initializeMiddlewares(){
-        this.app.use(bodyParser.json());
+        this.app.use(express.json());
     }
     
     private initializeControllers(controllers: any[]){
--- src/controllers/tasks/taskController.ts
+++ src/controllers/tasks/taskController.ts
@@ -18,33 +18,54 @@ class taskController{
 
         this.router.get(this.path, this.getAllTasks);/** creating a get request path and collback function for it */
         this.router.post(`${this.path}/create`, this.createTask);
-        this.router.delete("/tasks/delete", this.deleteTask);
+        this.router.delete("/tasks/delete/:id", this.deleteTask);
     }
 
     createTask=(request: express.Request, response: express.Response)=>{
-        const reqData = request.body;
-        const createdTask = new this.task(reqData);
-        createdTask.save().then(task=>{
-            console.log("created task:: ",task);
+        try{
+            const reqData = request.body;
+            const createdTask = new this.task(reqData);
+            createdTask.save().then(task=>{
             response.send(task);
         });
+        }catch(error){
+            console.log("Error in creating task:: ",error);
+            response.status(401).send("Some Error Occurred");
+        };
+        
     }
 
     getAllTasks=(request: express.Request, response: express.Response)=>{
-        console.log("delete request made")
-        this.task.find().then(tasks=>{
-            response.send(tasks);
-        })
+        try{
+            this.task.find().then(tasks=>{
+                response.status(200).send(tasks);
+            })
+        }catch(error){
+            console.log("Error in fetching all tasks:: ",error);
+            response.status(401).send({
+                "message":"Some error occured"
+            })
+        }
     }
     
-    deleteTask=(request: express.Request, response: express.Response)=>{
-        console.log("delete request made")
-        const id = request.params.id;
-        console.log("id is:: ",id);
+    deleteTask=async (request: express.Request, response: express.Response)=>{
+        try{
+            const id = request.params.id;
+            const returnedTask = await this.task.findOneAndDelete({
+                id: id
+            })
+            response.status(200).send({
+                "message": "Task Successfully deleted",
+            });
+        }catch(error){
+            console.log("Error in deleting tasks:: ",error);
+            response.status(401).send({
+                "message":"Some error occured"
+            })
+        }
         
-
-
     }
+
 
 }
 
