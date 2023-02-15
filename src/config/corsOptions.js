82441c045c2d20f771562d8dd5d1410e1bd367c6
satyam4p <satyamkumar343@gmail.com>
2023-02-16 01:13:53 +0530

    fix: allowing origins

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,13 +1,15 @@
 import { allowedOrigins } from "./allowedOrigins";
 
 export const corsOptions = {
-    origin: (origin, callback)=>{
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
-            callback(null, true);
-        }else{
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    // origin: (origin, callback)=>{
+    //     if(allowedOrigins.indexOf(origin) !== -1 || !origin){
+    //         callback(null, true);
+    //     }else{
+    //         callback(new Error('Not allowed by CORS'));
+    //     }
+    // },
+    origin:'*',
+    allowedOrigins:"*",
     credentials: true,
     optionsSuccessStatus: 200,
     methods: "GET, POST, PUT, PATCH"
