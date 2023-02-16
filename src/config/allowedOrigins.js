b004921bcefbe47c2f28ab25f91ad264d0e706e0
satyam4p <satyamkumar343@gmail.com>
2023-02-16 10:35:45 +0530

    cors issue changes

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 export const allowedOrigins = [
+    'https://orgtaskboard.netlify.app',
+    'http://orgtaskboard.netlify.app',
     'http://localhost:9001',
     'http://localhost:3000',
-    'https://orgtaskboard.netlify.app'
 ]
--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,15 +1,15 @@
 import { allowedOrigins } from "./allowedOrigins";
 
 export const corsOptions = {
-    // origin: (origin, callback)=>{
-    //     if(allowedOrigins.indexOf(origin) !== -1 || !origin){
-    //         callback(null, true);
-    //     }else{
-    //         callback(new Error('Not allowed by CORS'));
-    //     }
-    // },
-    origin:'*',
-    allowedOrigins:"*",
+    origin: (origin, callback)=>{
+        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
+            console.log("origin found");
+            callback(null, true);
+        }else{
+            console.log("origin found");
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
     optionsSuccessStatus: 200,
     methods: "GET, POST, PUT, PATCH"
