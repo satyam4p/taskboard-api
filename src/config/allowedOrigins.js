abc2fb4c10bce6a55443abb651ae6e222d4a1bc6
satyam4p <satyamkumar343@gmail.com>
2023-07-20 17:41:28 +0530

    added origin

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 export const allowedOrigins = [
     'https://orgtaskboard.netlify.app',
     'http://orgtaskboard.netlify.app',
+    'http://localhost:8080',
     'http://localhost:9001',
     'http://localhost:3000',
 ]
